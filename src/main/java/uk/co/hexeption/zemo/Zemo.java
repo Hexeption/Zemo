79baa31df3f2ab0b6b661a6d014ce80495aae708
Hexeption <minecraftfun201@gmail.com>
2019-03-30 22:27:31 +0000

    Added Forge MKD

---
+++ src/main/java/uk/co/hexeption/zemo/Zemo.java
@@ -0,0 +1,14 @@
+package uk.co.hexeption.zemo;
+
+import net.minecraftforge.fml.common.Mod;
+
+public class Zemo {
+
+    public static final String MOD_ID = "zemo";
+    public static final String MOD_NAME = "Zemo";
+    public static final String VERSION = "1.0-SNAPSHOT";
+
+    @Mod.Instance(MOD_ID)
+    public static Zemo INSTANCE;
+
+}
